e0fa3b81dafaaf4aa9362a3bbdbaf84efb8d220c
Tutmic Sedina <sedina.tutmic.23@size.ba>
2025-06-14 09:46:02 +0200

    AZUSJ-148: Dodana UI komponenta za prikaz rasporeda časova

---
+++ language-learning-app/src/HomePage.jsx
@@ -0,0 +1,10 @@
+﻿import ScheduleComponent from './components/ScheduleComponent';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Početna stranica</h1>
+      <ScheduleComponent />
+    </div>
+  );
+}
---
+++ language-learning-app/src/components/ScheduleComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ScheduleComponent = () => {
+  return (
+    <div>
+      <h2>Tvoj sedmični raspored</h2>
+      <p>Prikaz časova i lekcija biće ovdje.</p>
+    </div>
+  );
+};
+
+export default ScheduleComponent;
